af8a04e6718a064ad427a673cd8134a68521666f
Lukasz Ozga <lukaszozga1324@gmail.com>
2022-12-04 18:44:07 +0000

    Add wrapping components, render list of objects as card

--- src/App.js
+++ src/App.js
@@ -2,18 +2,17 @@ import { Route, Routes } from "react-router-dom";
 import AllMeetups from "./pages/AllMeetups";
 import Favourites from "./pages/Favourites";
 import NewMeetup from "./pages/NewMeetup";
-import Navbar from "./components/layout/Navbar";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
-    <div>
-      <Navbar />
+    <Layout>
       <Routes>
         <Route path="/" element={<AllMeetups />} />
         <Route path="/new-meetup" element={<NewMeetup />} />
         <Route path="/favourites" element={<Favourites />} />
       </Routes>
-    </div>
+    </Layout>
   );
 }
 
