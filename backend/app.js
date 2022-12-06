abf5e41e6f490087a2dc85cc8460970d2a2272c4
Lukasz Ozga <lukaszozga1324@gmail.com>
2022-12-06 02:32:50 +0000

    Add dockerised backend for React app

---
+++ backend/app.js
@@ -0,0 +1,63 @@
+const sqlite3 = require("sqlite3").verbose();
+const express = require("express");
+const path = require('node:path');
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+let db = new sqlite3.Database( path.resolve(__dirname, 'db.sqlite'), (err) => {
+  if (err) {
+    console.error(err.message);
+    throw err;
+  } else {
+    console.log("Connected to the sample database.");
+    db.run(
+      "CREATE TABLE IF NOT EXISTS meetups(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, url TEXT, address TEXT, description TEXT)"
+    );
+    console.log("Created meetups table.");
+  }
+});
+
+app.get("/meetups/", (req, res, next) => {
+  var sql = "SELECT * FROM meetups";
+  var params = [];
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: rows,
+    });
+  });
+});
+
+app.post("/meetups/", (req, res) => {
+  db.serialize(() => {
+    db.run(
+      "INSERT INTO meetups(id,title,url,address,description) VALUES(?,?,?,?,?)",
+      [
+        req.body.id,
+        req.body.title,
+        req.body.url,
+        req.body.address,
+        req.body.description,
+      ],
+      function (err) {
+        if (err) {
+          res.status(400).json({ error: err.message });
+          return;
+        }
+        res.status(201).json({
+          id: this.lastID,
+        });
+      }
+    );
+  });
+});
+
+app.listen(8000, () => {
+  console.log("Server listening on port: 8000.");
+});
